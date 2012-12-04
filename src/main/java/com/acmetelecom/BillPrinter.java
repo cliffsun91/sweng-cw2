0d6af960a0076c5532931e61ad837c78a2e9d019
Abz <abz.chand@gmail.com>
2012-12-04 01:47:56 +0000

    changes made beyond everyone's understanding

--- src/main/java/com/acmetelecom/BillPrinter.java
+++ src/main/java/com/acmetelecom/BillPrinter.java
@@ -6,25 +6,25 @@ import com.acmetelecom.customer.Customer;
 
 public class BillPrinter {
 
-    private IMoneyFormatter moneyFormatter;
+	private IMoneyFormatter moneyFormatter;
 
-    BillPrinter(IMoneyFormatter moneyFormatter){
-        this.moneyFormatter = moneyFormatter;
-    }
+	BillPrinter(IMoneyFormatter moneyFormatter){
+		this.moneyFormatter = moneyFormatter;
+	}
 
-    public void print(final Customer customer,
-                      final List<LineItem> calls, final String totalBill,
-                      Printer printer) {
+	public void print(final Customer customer,
+			final List<LineItem> calls, final String totalBill,
+			Printer printer) {
 
-        printer.printHeading(customer.getFullName(), customer.getPhoneNumber(),
-                customer.getPricePlan());
+		printer.printHeading(customer.getFullName(), customer.getPhoneNumber(),
+				customer.getPricePlan());
 
-        for (final LineItem call : calls) {
-            printer.printItem(call.date(), call.callee(), call.durationMinutes(),
-                    moneyFormatter.penceToPounds(call.cost()));
-        }
+		for (final LineItem call : calls) {
+			printer.printItem(call.date(), call.callee(), call.durationMinutes(),
+					moneyFormatter.penceToPounds(call.cost()));
+		}
 
-        printer.printTotal(totalBill);
-    }
+		printer.printTotal(totalBill);
+	}
 
 }
--- src/main/java/com/acmetelecom/BillingSystem.java
+++ src/main/java/com/acmetelecom/BillingSystem.java
@@ -17,95 +17,105 @@ import com.acmetelecom.customer.Tariff;
 
 public class BillingSystem {
 
-  private final List<AbstractCallEvent> callLog = new ArrayList<AbstractCallEvent>();
+	private final List<AbstractCallEvent> callLog = new ArrayList<AbstractCallEvent>();
 
-  public void callInitiated(final String caller, final String callee) {
-    callLog.add(new CallStart(caller, callee));
-  }
+	public void callInitiated(final String caller, final String callee) {
+		callLog.add(new CallStart(caller, callee));
+	}
 
-  public void callCompleted(final String caller, final String callee) {
-    callLog.add(new CallEnd(caller, callee));
-  }
+	public void callCompleted(final String caller, final String callee) {
+		callLog.add(new CallEnd(caller, callee));
+	}
 
-  public void createCustomerBills() {
-    final List<Customer> customers = CentralCustomerDatabase.getInstance()
-        .getCustomers();
-    for (final Customer customer : customers) {
-      createBillFor(customer);
-    }
-    callLog.clear();
-  }
+	public void createCustomerBills() {
+		final List<Customer> customers = CentralCustomerDatabase.getInstance()
+		.getCustomers();
+		for (final Customer customer : customers) {
+			createBillFor(customer);
+		}
+		callLog.clear();
+	}
 
-  private void createBillFor(final Customer customer) {
-    final List<AbstractCallEvent> customerEvents = new ArrayList<AbstractCallEvent>();
-    for (final AbstractCallEvent callEvent : callLog) {
-      if (callEvent.getCaller().equals(customer.getPhoneNumber())) {
-        customerEvents.add(callEvent);
-      }
-    }
+	private void createBillFor(final Customer customer) {
+		final List<AbstractCallEvent> customerEvents = getCustomerEvents(customer);
 
-    final List<Call> calls = new ArrayList<Call>();
+		final List<Call> calls = getCalls(customerEvents);
 
-    CallEvent start = null;
-    for (final CallEvent event : customerEvents) {
-      if (event instanceof CallStart) {
-        start = event;
-      }
-      if (event instanceof CallEnd && start != null) {
-        calls.add(new Call(start, event));
-        start = null;
-      }
-    }
+		BigDecimal totalBill = new BigDecimal(0);
+		final List<LineItem> items = new ArrayList<LineItem>();
 
-    BigDecimal totalBill = new BigDecimal(0);
-    final List<LineItem> items = new ArrayList<LineItem>();
+		for (final Call call : calls) {
 
-    for (final Call call : calls) {
+			final Tariff tariff = CentralTariffDatabase.getInstance().tarriffFor(
+					customer);
 
-      final Tariff tariff = CentralTariffDatabase.getInstance().tarriffFor(
-          customer);
+			BigDecimal cost;
 
-      BigDecimal cost;
+			final DaytimePeakPeriod peakPeriod = new DaytimePeakPeriod();
+			cost = calculateCost(call, peakPeriod, tariff);
 
-      final DaytimePeakPeriod peakPeriod = new DaytimePeakPeriod();
-      cost = calculateCost(call, peakPeriod, tariff);
+			cost = cost.setScale(0, RoundingMode.HALF_UP);
+			final BigDecimal callCost = cost;
+			totalBill = totalBill.add(callCost);
+			items.add(new DefaultLineItem(call, callCost));
+		}
 
-      cost = cost.setScale(0, RoundingMode.HALF_UP);
-      final BigDecimal callCost = cost;
-      totalBill = totalBill.add(callCost);
-      items.add(new DefaultLineItem(call, callCost));
-    }
+		String totalBillString = new MoneyFormatter().penceToPounds(totalBill);
+		new BillPrinter(new MoneyFormatter()).print(customer, items,
+				totalBillString, HtmlPrinter.getInstance());
+	}
 
-    String totalBillString = new MoneyFormatter().penceToPounds(totalBill);
-    new BillPrinter(new MoneyFormatter()).print(customer, items,
-            totalBillString, HtmlPrinter.getInstance());
-  }
+	private List<Call> getCalls(final List<AbstractCallEvent> customerEvents) {
+		final List<Call> calls = new ArrayList<Call>();
 
-  private BigDecimal calculateCost(final Call call,
-      final DaytimePeakPeriod peakPeriod, final Tariff tariff) {
+		CallEvent start = null;
+		for (final CallEvent event : customerEvents) {
+			if (event instanceof CallStart) {
+				start = event;
+			}
+			if (event instanceof CallEnd && start != null) {
+				calls.add(new Call(start, event));
+				start = null;
+			}
+		}
+		return calls;
+	}
 
-    /*
-     * NEW FUNCTIONALITY - let's look at old functionality first... // Only
-     * charge peak rate during peak times final int durationOffPeak =
-     * peakPeriod.calculateOffPeakTime(call.startTime(), call.endTime(),
-     * call.durationSeconds()); final int durationPeak =
-     * peakPeriod.calculatePeakTime(call.startTime(), call.endTime(),
-     * call.durationSeconds());
-     * 
-     * return new
-     * BigDecimal(durationOffPeak).multiply(tariff.offPeakRate()).add( new
-     * BigDecimal(durationPeak).multiply(tariff.peakRate()));
-     */
+	private List<AbstractCallEvent> getCustomerEvents(final Customer customer) {
+		final List<AbstractCallEvent> customerEvents = new ArrayList<AbstractCallEvent>();
+		for (final AbstractCallEvent callEvent : callLog) {
+			if (callEvent.getCaller().equals(customer.getPhoneNumber())) {
+				customerEvents.add(callEvent);
+			}
+		}
+		return customerEvents;
+	}
 
-    if (peakPeriod.offPeak(call.startTime())
-        && peakPeriod.offPeak(call.endTime())
-        && call.durationSeconds() < 12 * 60 * 60) {
-      return new BigDecimal(call.durationSeconds()).multiply(tariff
-          .offPeakRate());
-    } else {
-      return new BigDecimal(call.durationSeconds()).multiply(tariff.peakRate());
-    }
+	private BigDecimal calculateCost(final Call call,
+			final DaytimePeakPeriod peakPeriod, final Tariff tariff) {
 
-  }
+		/*
+		 * NEW FUNCTIONALITY - let's look at old functionality first... // Only
+		 * charge peak rate during peak times final int durationOffPeak =
+		 * peakPeriod.calculateOffPeakTime(call.startTime(), call.endTime(),
+		 * call.durationSeconds()); final int durationPeak =
+		 * peakPeriod.calculatePeakTime(call.startTime(), call.endTime(),
+		 * call.durationSeconds());
+		 * 
+		 * return new
+		 * BigDecimal(durationOffPeak).multiply(tariff.offPeakRate()).add( new
+		 * BigDecimal(durationPeak).multiply(tariff.peakRate()));
+		 */
+
+		if (peakPeriod.offPeak(call.startTime())
+				&& peakPeriod.offPeak(call.endTime())
+				&& call.durationSeconds() < 12 * 60 * 60) {
+			return new BigDecimal(call.durationSeconds()).multiply(tariff
+					.offPeakRate());
+		} else {
+			return new BigDecimal(call.durationSeconds()).multiply(tariff.peakRate());
+		}
+
+	}
 
 }
