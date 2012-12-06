0503556fb7aac9e23cc9fee41dfc8e2f9f5f2bd0
Osama Javed <oaj09@ic.ac.uk>
2012-12-06 01:25:41 +0000

    should be fixed now

--- src/main/java/com/acmetelecom/TimeUtils/ITimeCalculator.java
+++
@@ -1,8 +0,0 @@
-package com.acmetelecom.TimeUtils;
-
-import org.joda.time.DateTime;
-
-public interface ITimeCalculator{
-
-	PeakOffPeakTime calculateTimes(DateTime startTime, DateTime endTime);
-}
--- src/main/java/com/acmetelecom/timeUtils/ITimeCalculator.java
+++
@@ -1,8 +0,0 @@
-package com.acmetelecom.timeUtils;
-
-import org.joda.time.DateTime;
-
-public interface ITimeCalculator{
-
-	PeakOffPeakTime calculateTimes(DateTime startTime, DateTime endTime);
-}
