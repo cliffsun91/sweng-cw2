e73df06a8f0b7deb84135cc8a6fbd9fcb1ee99b7
Abz <abz.chand@gmail.com>
2012-12-06 21:35:11 +0000

    Removed some logic from billing system into another class

--- src/main/java/com/acmetelecom/BillingSystem.java
+++ src/main/java/com/acmetelecom/BillingSystem.java
@@ -1,21 +1,17 @@
 package com.acmetelecom;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import org.joda.time.DateTime;
 
-import com.acmetelecom.billcalculator.TotalBillCalculator;
+import com.acmetelecom.billcalculator.CustomerBillGenerator;
 import com.acmetelecom.call.CallTime;
-import com.acmetelecom.call.LineItem;
 import com.acmetelecom.customer.CentralCustomerDatabase;
 import com.acmetelecom.customer.CentralTariffDatabase;
 import com.acmetelecom.customer.Customer;
 import com.acmetelecom.customer.Tariff;
-import com.acmetelecom.moneyformatters.MoneyFormatter;
-import com.acmetelecom.printer.BillGenerator;
 import com.acmetelecom.printer.HtmlPrinter;
 import com.acmetelecom.printer.Printer;
 import com.acmetelecom.timeutils.ITimeCalculator;
@@ -24,8 +20,7 @@ import com.acmetelecom.timeutils.TimeCalculator;
 public class BillingSystem {
 
 	private final HashMap<String, List<CallTime>> customerCurrentCallLog;
-	private final BillGenerator billGenerator;
-	private final TotalBillCalculator totalBillCalculator;
+	private final CustomerBillGenerator customerBillGenerator;
 
 	public BillingSystem() {
 		this(HtmlPrinter.getInstance());
@@ -37,8 +32,7 @@ public class BillingSystem {
 	
 	public BillingSystem(Printer printer, ITimeCalculator timeCalculator) {
 		this.customerCurrentCallLog = new HashMap<String, List<CallTime>>();
-		this.totalBillCalculator = new TotalBillCalculator(timeCalculator);
-		billGenerator = new BillGenerator(new MoneyFormatter(), printer);
+		this.customerBillGenerator = new CustomerBillGenerator(printer, timeCalculator);
 	}
 
 	public void callInitiated(String caller, String callee){
@@ -56,6 +50,19 @@ public class BillingSystem {
 		time.setEndTime(DateTime.now());
 	}
 	
+	public void createCustomerBills(){
+		createCustomerBills(CentralCustomerDatabase.getInstance().getCustomers());
+	}
+
+	private void createCustomerBills(final List<Customer> customers) {
+		for (final Customer customer : customers) {
+			final List<CallTime> calls = customerCurrentCallLog.get(customer.getPhoneNumber());
+			final Tariff tariff = CentralTariffDatabase.getInstance().tarriffFor(customer);
+			customerBillGenerator.createBillFor(customer, calls, tariff);
+			customerCurrentCallLog.put(customer.getPhoneNumber(), new ArrayList<CallTime>());
+		}
+	}
+	
 	public void fullCompletedCall(CallTime call){
 		String caller = call.getCaller();
 		if (customerCurrentCallLog.get(caller) == null){
@@ -63,28 +70,5 @@ public class BillingSystem {
 		}
 		List<CallTime> calls = customerCurrentCallLog.get(caller);
 		calls.add(call);
-	}
-	
-	public void createCustomerBills(){
-		createCustomerBills(CentralCustomerDatabase.getInstance().getCustomers());
-	}
-
-	public void createCustomerBills(final List<Customer> customers) {
-		for (final Customer customer : customers) {
-			createBillFor(customer);
-			customerCurrentCallLog.put(customer.getPhoneNumber(), new ArrayList<CallTime>());
-		}
-	}
-
-	private void createBillFor(final Customer customer) {	
-		final List<LineItem> items = new ArrayList<LineItem>();
-		final Tariff tariff = CentralTariffDatabase.getInstance().tarriffFor(customer);
-		
-		List<CallTime> calls = customerCurrentCallLog.get(customer.getPhoneNumber());
-		BigDecimal totalBill = totalBillCalculator.calculateTotalBill(calls, tariff, items);
-
-		String totalBillString = new MoneyFormatter().penceToPounds(totalBill);
-
-		billGenerator.print(customer, items, totalBillString);
 	}
 }
